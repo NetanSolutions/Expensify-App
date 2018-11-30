ca3744cea59d4e59ef9ecae7608032562aea82d7
Paul Duffy <paul.duffy@netan.solutions>
2018-11-30 15:31:47 +0000

    Set up production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,55 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 
-module.exports = {
-    entry: ['babel-polyfill', './src/index.js'],
-    output: {
-        path: path.resolve(__dirname, 'public/scripts'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['env','react'],
-                    plugins: ['transform-object-rest-spread', 'transform-class-properties']
+module.exports = (env) => {
+    const isProduction = env === 'production'
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' })
+    
+    return {
+        entry: ['babel-polyfill', './src/index.js'],
+        output: {
+            path: path.resolve(__dirname, 'public/scripts'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env','react'],
+                        plugins: ['transform-object-rest-spread', 'transform-class-properties']
+                    }
                 }
-            }
-        }, {
-            test: /\.s?css$/,
-            use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-        }]
-    },
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-        historyApiFallback: true,
-        publicPath: '/scripts/'
-    },
-    devtool: 'source-map'
+            }, {
+                test: /\.s?css$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }, {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        devServer: {
+            contentBase: path.resolve(__dirname, 'public'),
+            historyApiFallback: true,
+            publicPath: '/scripts/'
+        },
+    }
+    
 }
